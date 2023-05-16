d1f45424001efabf9296eec109f246f2d7acec3a
faisalinfinity <mujtabafaisal944.fk@gmail.com>
2023-05-16 22:08:58 +0530

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+
 
 export default function App() {
+  const [state, setState] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View >
+        <Text>Counter {state}</Text>
+      </View>
+      <View style={styles.button}>
+        <Button
+          color={"purple"}
+          onPress={() => setState((prev) => prev + 1)}
+          title="Increase Count"
+        ></Button>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    fontSize: "20px",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "pink",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    fontSize: "2px",
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 10,
+    overflow: "hidden",
   },
 });
