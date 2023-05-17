a6bacf155f872b7dada5ef113bc0f779470fff40
faisalinfinity <mujtabafaisal944.fk@gmail.com>
2023-05-17 16:25:46 +0530

    first

--- App.js
+++ App.js
@@ -1,40 +1,66 @@
-import { Button, StyleSheet, Text, View } from "react-native";
-import { useState } from "react";
+import React, { useState,useEffect } from 'react';
+import { View, Text, TextInput, Button, Alert } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+import PushNotification from 'react-native-push-notification';
 
+const App = () => {
+  const [reminderText, setReminderText] = useState('');
 
-export default function App() {
-  const [state, setState] = useState(0);
+  useEffect(() => {
+    if(PushNotification){
+      PushNotification.createChannel(
+        {
+          channelId: 'reminder-channel', // Unique channel ID
+          channelName: 'Reminders', // Channel name
+          channelDescription: 'Channel for reminders', // Channel description
+          soundName: 'default', // Sound to play for notifications
+          importance: 4, // Notification importance (default value: 4)
+          vibrate: true, // Vibrate on notification
+        },
+        (created) => console.log(`Channel creation ${created ? 'success' : 'failed'}`)
+      );
+    }
+   
+  }, []);
+
+  const handleSaveReminder = async () => {
+    if (reminderText.trim() === '') {
+      Alert.alert('Error', 'Please enter a reminder text');
+      return;
+    }
+
+    try {
+      const reminders = await AsyncStorage.getItem('reminders');
+      let parsedReminders = reminders ? JSON.parse(reminders) : [];
+      parsedReminders.push(reminderText);
+
+      await AsyncStorage.setItem('reminders', JSON.stringify(parsedReminders));
+      setReminderText('');
+
+      // Schedule local notification
+      PushNotification.localNotification({
+        channelId: 'reminder-channel', // Channel ID for the notification
+        message: `Reminder: ${reminderText}`,
+      });
+
+      Alert.alert('Success', 'Reminder saved successfully');
+    } catch (error) {
+      Alert.alert('Error', 'Failed to save the reminder');
+    }
+  };
 
   return (
-    <View style={styles.container}>
-      <View >
-        <Text>Counter {state}</Text>
-      </View>
-      <View style={styles.button}>
-        <Button
-          color={"purple"}
-          onPress={() => setState((prev) => prev + 1)}
-          title="Increase Count"
-        ></Button>
-      </View>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ fontSize: 18, marginBottom: 10 }}>Enter Reminder:</Text>
+      <TextInput
+        style={{ height: 40, width: 250, borderColor: 'gray', borderWidth: 1, padding: 5 }}
+        onChangeText={(text) => setReminderText(text)}
+        value={reminderText}
+        placeholder="Enter your reminder text"
+      />
+      <Button title="Save Reminder" onPress={handleSaveReminder} />
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    fontSize: "20px",
-    flex: 1,
-    backgroundColor: "pink",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  button: {
-    fontSize: "2px",
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    borderRadius: 10,
-    overflow: "hidden",
-  },
-});
+export default App;
